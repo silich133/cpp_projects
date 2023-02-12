6317eae0d17b538370ae020365f4e1f691a6b21f
silichev133 <silich.133@yandex.ru>
2023-02-12 15:05:33 +0300

    New mini app with check vales of int variables

---
+++ itvdn/starter/SizeofTest/SizeofTest/SizeofTest.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 3456;
+    unsigned int c = 324234;
+    string s = "This is new string";
+
+    cout << a << '\t' << sizeof(a) << endl;
+    cout << b << '\t' << sizeof(b) << endl;
+    cout << c << '\t' << sizeof(c) << endl;
+    cout << s << '\t' << sizeof(s) << endl;
+
+    return 0;
+}
+
+
