a2fbead02ca560d73d402759e57e5c69607c2f11
silichev133 <silich.133@yandex.ru>
2023-02-12 15:38:13 +0300

    Added new variable

--- itvdn/starter/SizeofTest/SizeofTest/SizeofTest.cpp
+++ itvdn/starter/SizeofTest/SizeofTest/SizeofTest.cpp
@@ -10,11 +10,14 @@ int main()
     int b = 3456;
     unsigned int c = 324234;
     string s = "This is new string";
+    short months = 12;
 
     cout << a << '\t' << sizeof(a) << endl;
     cout << b << '\t' << sizeof(b) << endl;
     cout << c << '\t' << sizeof(c) << endl;
     cout << s << '\t' << sizeof(s) << endl;
+
+    cout << months << '\t' << sizeof(months) << endl;
 
     return 0;
 }
